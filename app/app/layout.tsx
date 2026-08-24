fc6528d8c6c309035131b8e21c48900d5819aacb
young8creator <benjaminesther723@gmail.com>
2026-08-24 06:33:23 -0700

    Create layout.tsx

---
+++ app/app/layout.tsx
@@ -0,0 +1,5 @@
+import './globals.css'
+export const metadata = { title: 'PortalStack' }
+export default function RootLayout({ children }) { 
+  return <html lang="en"><body>{children}</body></html> 
+}
